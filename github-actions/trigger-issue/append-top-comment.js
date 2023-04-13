7c1df4e94af688bc337012935ed84aba27df88aa
Ashelle Cancio <vermillion707@gmail.com>
2023-04-13 00:51:16 -0700

    refactored workflow file to include repo token

--- github-actions/trigger-issue/append-top-comment.js
+++ github-actions/trigger-issue/append-top-comment.js
@@ -12,26 +12,31 @@ async function main({g, c}) {
 	github = g
 	context = c
 
-	const octokit = new Octokit({ auth: secrets.HFLA_PROJECT_BOARD_TOKEN })
+	const octokit = new Octokit({ auth: process.env.REPO_TOKEN })
 	const issueNum = context.payload.issue.number
 	const wikiLink =
 		"https://github.com/hackforla/website/wiki/How-to-work-off-of-a-feature-branch";
-	
-	await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
-		owner: "arcan9",
-		repo: "website",
-		issue_number: issueNum,
-		title: "Instructions for Feature Branch",
-		body: `This issue involves working off of a feature branch. 
-		
-		Check out this [wiki](${wikiLink}) and follow the outlined steps.
-		
-		Note: Do not manually remove this instruction at the bottom.`,
-		state: "open",
-		headers: {
-			"X-GitHub-Api-Version": "2022-11-28"
-		}
-	});
+
+	try {
+		await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			issue_number: issueNum,
+			title: "Instructions for Feature Branch",
+			body: `This issue involves working off of a feature branch. 
+			
+			Check out this [wiki](${wikiLink}) and follow the outlined steps.
+			
+			Note: Do not manually remove this instruction at the bottom.`,
+			state: "open",
+			headers: {
+				"X-GitHub-Api-Version": "2022-11-28"
+			}
+		})
+	}
+	catch(err) {
+		throw new Error(err);
+	}
 }
 
 module.exports =  main;
